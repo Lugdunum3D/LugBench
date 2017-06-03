be12e9a18b54e85d062bef13e86685383d637be6
Yoann Long <yoann.long@epitech.eu>
2017-06-03 14:00:16 +0200

    adding Java class that inherit from NativeActivity to perform the native android request

---
+++ android/lugbench/src/main/java/com/lugdunum/lugbench/LugBenchNativeActivity.java
@@ -0,0 +1,60 @@
+package com.lugdunum.lugbench;
+
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LugBenchNativeActivity extends NativeActivity {
+    private final static String TAG = "LugBenchNativeActivity";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void sendRequest(final String url, final String json) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.v(TAG, response);
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v(TAG, "Failed to send the request");
+                    Log.v(TAG, error.networkResponse.toString());
+                }
+            }
+        ) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return json.getBytes();
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type","application/json");
+                // TODO : Version
+                return headers;
+            }
+        };
+
+        queue.add(stringRequest);
+    }
+}
+
