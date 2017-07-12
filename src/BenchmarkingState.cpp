8791b7728ab265dffb3b6965ef18f168c512e1da
Yoann Long <yoann.long@epitech.eu>
2017-07-12 10:33:28 +0200

    exit application rather than goin back to menu during benchmarking when window is closed

--- src/BenchmarkingState.cpp
+++ src/BenchmarkingState.cpp
@@ -94,7 +94,7 @@ bool BenchmarkingState::onPop() {
 
 void BenchmarkingState::onEvent(const lug::Window::Event& event) {
     if (event.type == lug::Window::Event::Type::Close) {
-       _application.popState();
+       _application.close();
     }
 }
 
